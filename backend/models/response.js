7e10451e1dd8e7dd941695d2bfdbaf6b710d00fe
klynx <KevinLynx_L@hotmail.com>
2018-12-01 11:44:45 -0800

    Deletes

--- backend/models/response.js
+++
@@ -1,7 +0,0 @@
-function Response(code, err, data)
-{
-    this.status = code;
-    this.errorMessage = (err)? ((err.message)? err.message : err) : "";
-    this.responseData = (data)? data : "";
-}
-module.exports = Response;
